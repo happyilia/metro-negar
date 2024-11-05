555a4c4d3d08b41f2d887f74cf6f8948521a1afd
Iliya <ilyabbasi@gmail.com>
2024-11-05 21:42:05 +0330

    some chnges in welcome window

--- project.py
+++ project.py
@@ -6,55 +6,38 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout 
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-class MyGrid(GridLayout):
-    def __init__(self,**kwargs):
-        super(MyGrid,self).__init__(**kwargs)
-        self.rows = 3
-
-        self.inside = GridLayout()
-        self.inside.cols = 3
-        self.inside.rows = 3
-
-        self.add_widget(Label(text=get_display(arabic_reshaper.reshape("خوش آمدید! ")),font_size = 50, font_name='arial' ))
-        self.add_widget(Label(text=get_display(arabic_reshaper.reshape("لطفا شهر خود را انتخاب کنید: ")),font_size = 40, font_name='arial') )
-
-        self.tehran_btn = Button(text=get_display(arabic_reshaper.reshape('تهران')), font_size=40,font_name='arial')
-        self.tehran_btn.bind(on_press=self.pressed)
-        self.inside.add_widget(self.tehran_btn)
-
-        self.mashhad_btn = Button(text=get_display(arabic_reshaper.reshape('مشهد')), font_size=40,font_name='arial')
-        self.mashhad_btn.bind(on_press=self.pressed)
-        self.inside.add_widget(self.mashhad_btn)
-
-        self.isfahan_btn = Button(text=get_display(arabic_reshaper.reshape('اصفهان')), font_size=40,font_name='arial')
-        self.isfahan_btn.bind(on_press=self.pressed)
-        self.inside.add_widget(self.isfahan_btn)
-
-        self.shiraz_btn = Button(text=get_display(arabic_reshaper.reshape('شیراز')), font_size=40,font_name='arial')
-        self.shiraz_btn.bind(on_press=self.pressed)
-        self.inside.add_widget(self.shiraz_btn)
-
-        self.tabriz_btn = Button(text=get_display(arabic_reshaper.reshape('تبریز')), font_size=40,font_name='arial')
-        self.tabriz_btn.bind(on_press=self.pressed)
-        self.inside.add_widget(self.tabriz_btn)
-
-        self.karaj_btn = Button(text=get_display(arabic_reshaper.reshape('کرج')), font_size=40,font_name='arial')
-        self.karaj_btn.bind(on_press=self.pressed)
-        self.inside.add_widget(self.karaj_btn)
-
-        self.add_widget(self.inside)
+kv = Builder.load_file("my.kv")
+Window.clearcolor = (1, 1, 1, 1)
 
 
+class WelcomeWindow(Screen):
     def pressed(self, intance):
         print("pressed")
+
+class TehranLineWin(Screen):
+    def pressed(self, intance):
+        print("pressed")
+
+class WindowManager(ScreenManager):
+    pass
+
+screens = [WelcomeWindow(name="welcome"),TehranLineWin(name="tehranline")]
+sm = WindowManager()
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "welcome"
 class MyApp (App):
     def build(self): 
-        return MyGrid()
+        return sm
         
         
 if __name__=="__main__":
     MyApp().run()
 
 
-print("metro-negar")
