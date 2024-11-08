40bf259a1584d678092f5eaa0a1518add03f9c6e
Iliya <ilyabbasi@gmail.com>
2024-11-08 11:46:34 +0330

    adding header

--- project.py
+++ project.py
@@ -10,6 +10,7 @@ from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.graphics import *
 
 kv = Builder.load_file("my.kv")
 Window.clearcolor = (1, 1, 1, 1)
@@ -62,26 +63,61 @@ class WelcomeWindow(Screen):
     def pressed(self, intance):
         print("pressed")
 class TehranLineWin(Screen):
-    def __init__(self,**kwargs):
-        super(TehranLineWin,self).__init__(**kwargs)
-        self.inside = FloatLayout(pos_hint={'x':0,'y':0},size_hint = (1, 0.1))
+    def __init__(self, **kwargs):
+        super(TehranLineWin, self).__init__(**kwargs)
+        
+        with self.canvas.before:
+            self.rect_color = Color(0.4, 0.4, 0.4, 1)
+            self.rect = Rectangle()
+        
+        self.bind(size=self.update_rect, pos=self.update_rect)
+        
+        self.inside_footer = FloatLayout(pos_hint={'x': 0, 'y': 0}, size_hint=(1, 0.1))
         self.footer = GridLayout()
-        settingBtn = Button(background_normal="icons8-settings-480.png")
-        apiMapBtn = Button(background_normal="mapapiicon.png")
-        mapBtn = Button(background_normal="mapicon.png")
-        navBtn = Button(background_normal="navicon.png")
-        lineBtn = Button(background_normal="lineicon.png")
-        self.footer.rows =1
+
+        settingBtn = Button(background_normal="icons8-settings-480.png", background_down='icons8-settings-4802.png')
+        apiMapBtn = Button(background_normal="mapapiicon.png", background_down='mapapiicon2.png')
+        mapBtn = Button(background_normal="mapicon.png", background_down='mapicon2.png')
+        navBtn = Button(background_normal="navicon.png", background_down='navicon2.png')
+        lineBtn = Button(disabled=True, background_disabled_normal="lineicon2.png")
+        
+        self.footer.rows = 1
         self.footer.cols = 5
+
         self.footer.add_widget(settingBtn)
         self.footer.add_widget(apiMapBtn)
         self.footer.add_widget(lineBtn)
         self.footer.add_widget(navBtn)
         self.footer.add_widget(mapBtn)
-        self.inside.add_widget(self.footer)
-        self.add_widget(self.inside)
+        self.inside_footer.add_widget(self.footer)
+        self.add_widget(self.inside_footer)
 
-class TehranSetting(Screen):
+        mapBtn.bind(on_press=self.map_pressed)
+        apiMapBtn.bind(on_press=self.apimap_pressed)
+        navBtn.bind(on_press=self.nav_pressed)
+        settingBtn.bind(on_press=self.setting_pressed)
+
+        inside_header = FloatLayout(pos_hint={'x': 0, 'y': 0.9}, size_hint=(1, 0.1))
+        headerLabel = Label(text=get_display(arabic_reshaper.reshape("فهرست خطوط")), font_size=50, font_name='Vazir-Bold', color="white", pos_hint={'x': 0, 'y': 0})
+        inside_header.add_widget(headerLabel)
+        self.add_widget(inside_header)
+
+    def update_rect(self, *args): 
+        self.rect.pos = (0, self.height * 0.9) # Adjust position to be 90% down the screen 
+        self.rect.size = (self.width, self.height * 0.1)
+
+    def map_pressed(self, instance):
+        sm.current = "tehranmap"
+    def apimap_pressed(self, instance):
+        sm.current = "tehranapimap"
+    def nav_pressed(self, instance):
+        sm.current = "tehrannav"
+    def setting_pressed(self, instance):
+        sm.current = "setting"
+
+        
+
+class Setting(Screen):
     pass
 class TehranAPIMap(Screen):
     pass
@@ -92,7 +128,8 @@ class TehranNav(Screen):
 class WindowManager(ScreenManager):
     pass
 
-screens = [WelcomeWindow(name="welcome"),TehranLineWin(name="tehranline")]
+screens = [WelcomeWindow(name="welcome"),TehranLineWin(name="tehranline"),TehranMap(name="tehranmap"),
+           TehranAPIMap(name="tehranapimap"),TehranNav(name="tehrannav"),Setting(name="setting")]
 sm = WindowManager()
 for screen in screens:
     sm.add_widget(screen)
