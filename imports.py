93560a728758c8dcc70c1544ba8c90756c1ef5bb
Iliya <ilyabbasi@gmail.com>
2024-11-27 08:54:30 +0330

    adding a file that has every imports

---
+++ imports.py
@@ -0,0 +1,24 @@
+import kivy
+import os
+import arabic_reshaper
+from bidi.algorithm import get_display
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.graphics import *
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.graphics import Ellipse, Color
+from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRoundFlatButton
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.button import MDRoundFlatIconButton
+from kivymd.uix.button import BaseButton
+from kivy.graphics import Line
+from kivy.uix.scrollview import ScrollView
